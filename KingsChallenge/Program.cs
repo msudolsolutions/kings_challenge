65c04e1fa4449545a4ceffc1f03067a80e071a0f
Marek Sudoł <msudolsolutions@gmail.com>
2025-01-15 00:55:26 +0100

    added questions and answers for them

--- KingsChallenge/Program.cs
+++ KingsChallenge/Program.cs
@@ -2,6 +2,7 @@
 using System.Text.Json.Serialization;
 
 Console.WriteLine("Hello, Kings Challenge project!");
+var kings = new List<King>();
 string url = "https://gist.githubusercontent.com/christianpanton/10d65ccef9f29de3acd49d97ed423736/raw/b09563bc0c4b318132c7a738e679d4f984ef0048/kings"; // Replace with your URL
 using HttpClient client = new();
 try
@@ -17,18 +18,24 @@ try
     Console.WriteLine("Data fetched successfully!");
 
     // Deserialize the JSON array into a List of MyItem objects
-    var kings = JsonSerializer.Deserialize<List<King>>(responseData);
-
-    // Iterate over the list and process each item
-    foreach (var king in kings)
-    {
-        Console.WriteLine($"Id: {king.Id}, Name: {king.Name}, Country: {king.Country}, House: {king.House}, Years: {king.Years}");
-    }
+    kings = JsonSerializer.Deserialize<List<King>>(responseData);
 }
 catch (Exception ex)
 {
     Console.WriteLine($"An error occurred: {ex.Message}");
 }
+
+/*
+1. How many monarchs are there in the list?
+2. Which monarch ruled the longest (and for how long)?
+3. Which house ruled the longest (and for how long)?
+4. What was the most common first name?
+*/
+
+Console.WriteLine($"1. There are {kings.Count} monarchs in the list.");
+Console.WriteLine($"2. The monarch who ruled the longest is {kings.OrderByDescending(k => k.Years).First().Name} for {kings.OrderByDescending(k => k.Years).First().Years} years.");
+Console.WriteLine($"3. The house that ruled the longest is {kings.GroupBy(k => k.House).OrderByDescending(g => g.Count()).First().Key} for {kings.GroupBy(k => k.House).OrderByDescending(g => g.Count()).First().Count()} years.");
+Console.WriteLine($"4. The most common first name is {kings.GroupBy(k => k.Name.Split(' ')[0]).OrderByDescending(g => g.Count()).First().Key}.");
 
 public class King
 {
