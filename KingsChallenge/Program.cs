4142f0386ca0a92f073d65490025914e63d52b25
Marek Sudoł <msudolsolutions@gmail.com>
2025-01-15 00:31:31 +0100

    class for king is created

--- KingsChallenge/Program.cs
+++ KingsChallenge/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Kings Challenge project!");
+var kings = new List<King>();
+
+public class King
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public string House { get; set; }
+    public string Years { get; set; }
+}
